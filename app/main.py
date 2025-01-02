ebf0ef8f2803eb5e741def3dbeee6dad6208dc77
abhimessi16 <piabhi18@gmail.com>
2025-01-02 15:04:14 +0530

    feature: summarize input and get fact check output

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from googleapiclient import discovery
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import pipeline
 
 from app.routes.fact_check_route import fact_check_router
 from app.params import google_fact_check_tools_api_key
@@ -9,8 +9,7 @@ from app.params import google_fact_check_tools_api_key
 google_fact_check_service = discovery.build(
     "factchecktools", "v1alpha1", developerKey=google_fact_check_tools_api_key)
 
-model = T5ForConditionalGeneration.from_pretrained("t5-small")
-tokenizer = T5Tokenizer.from_pretrained("t5-small")
+summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 
 fact_checks_app = FastAPI()
 fact_checks_app.include_router(fact_check_router)
--- app/utils/google_utils.py
+++ app/utils/google_utils.py
@@ -1,24 +1,21 @@
 import app.main as session
-from app.google_models import ClaimSearch
 
 def fact_check(news_to_check: str):
     service = session.google_fact_check_service
-    model_prompt = "Convert the statement '' to True or False. Give a single word as 'True' or 'False'."
     is_fake_news = False
     try:
-        claims_resource = service.claims()
-        claims: ClaimSearch = claims_resource.search(query=news_to_check)
+        news_keywords = session.summarizer(news_to_check, max_length=20, min_length=10, do_sample=False)
+        claims = service.claims().search(query=news_keywords[0].get("summary_text", news_to_check)).execute()
         # for now let's check only the first page
         # also assuming that the results are actually relevant
-        for claim in claims.claims:
+        for claim in claims.get("claims", []):
             # lets take first review for now
-            claim_review = claim.claim_review[0]
-            inputs = session.tokenizer(model_prompt.format(claim_review.textual_rating), return_tensors="pt")
+            claim_review = claim.get("claimReview", [])
+            if not claim_review:
+                return is_fake_news
 
-            outputs = session.model.generate(inputs['input_ids'], max_length=10)
-            result = session.tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-            is_fake_news = is_fake_news or (True if result == "True" else False)
+            # there's multiple categories like half true, misleading, false, true, etc. For simplicity we check only true or false
+            is_fake_news = is_fake_news or (False if "true" in claim_review[0].get("textualRating", "").lower() else True)
             
     except Exception as ex:
         print(ex)
